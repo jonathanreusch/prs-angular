6a8cecd9429669d1fa12284443de47d607d1b502
Jonathan Reusch <jonathanreusch1@gmail.com>
2022-03-14 20:24:17 -0400

    added user dettail components

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,8 +4,6 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
 
-
-
 // configure base url
 const userRoute = '/users';
 
@@ -13,14 +11,18 @@ const userRoute = '/users';
   providedIn: 'root',
 })
 export class UserService {
-
-  url = environment.apiBaseUrl + userRoute
+  url = environment.apiBaseUrl + userRoute;
   constructor(private http: HttpClient) {}
 
-//http://localhost:8080/users/
+  //http://localhost:8080/users/
   getAll(): Observable<User[]> {
-    let requestUrl = this.url + '/'
-    return this.http.get<User[]>(requestUrl)
+    let requestUrl = this.url + '/';
+    return this.http.get<User[]>(requestUrl);
+  }
 
+  // http://localhost:8080/user/{id}
+  getById(id: number): Observable<User[]> {
+    let requestUrl = this.url + '/' + id;
+    return this.http.get<User[]>(requestUrl);
   }
 }
